3e37a5dcab3bf9483c195bf9beedfab5ed9cef0b
Leszek <leszek@strixspace.com>
2022-07-05 14:50:48 +0200

    :lipstick: Correcting typos and minor shortcomings

--- components/Author.vue
+++ components/Author.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="author" aria-labelledby="author-title" class="relative scroll-mt-14 pb-3 pt-8 sm:scroll-mt-32 sm:pb-16 sm:pt-10 lg:pt-16">
     <div class="relative mx-auto max-w-5xl pt-16 sm:px-6">
-      <div class="bg-slate-50 pt-px sm:rounded-6xl">
+      <div class="bg-slate-50 pt-px sm:rounded-6xl text-justify">
         <div class="relative mx-auto -mt-16 h-44 w-44 overflow-hidden rounded-full bg-slate-200 md:float-right md:h-64 md:w-64 md:[shape-outside:circle(40%)] lg:mr-20 lg:h-72 lg:w-72">
           <img alt="" src="~/assets/img/avatar.jpg">
         </div>
@@ -10,7 +10,7 @@
             <span class="text-base font-medium tracking-tight">O autorze</span>
           </h2>
           <p class="mt-8 font-display text-5xl font-extrabold tracking-tight text-slate-900 sm:text-6xl">
-            <span class="block text-blue-600">Leszek W. Król</span> Poznaj techniki zarządzania sobą w czasie
+            Nazywam się <span class="block text-blue-600">Leszek W. Król</span>
           </p>
           <p class="mt-4 text-lg tracking-tight text-slate-700">Inicjator i założyciel <a href="https://universityofgames.net" target="_blank" title="University of Games - Społeczność twórców gier" class="underline"><strong>University of Games</strong><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" class="inline ml-1"><g fill="#8c8c8c"><path d="M8.293 2.293L4.586 6 6 7.414l3.707-3.707L12 6V0H6l2.293 2.293z"></path><path d="M10 10H2V2h2V0H2a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2V8h-2z"></path></g></svg></a>  - międzynarodowego wirtualnego hubu dla niezależnych twórców gier komputerowych, którego celem jest dostarczanie wiedzy oraz praktycznych rozwiązań w obszarze projektowania i programowania gier komputerowych, w tym najlepszych praktyk biznesowych popartych historią sukcesu innych twórców z całego świata.</p>
           <p class="mt-4 text-lg tracking-tight text-slate-700">Twórca <a href="https://napiszprace.pl" target="_blank" title="Napisz Pracę - Wybierz jedno z setek wyzwań z którymi mierzą się przedsiębiorstwa w twojej okolicy." class="underline"><strong>NapiszPrace.pl</strong><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" class="inline ml-1"><g fill="#8c8c8c"><path d="M8.293 2.293L4.586 6 6 7.414l3.707-3.707L12 6V0H6l2.293 2.293z"></path><path d="M10 10H2V2h2V0H2a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2V8h-2z"></path></g></svg></a> - startupu z branży edukacyjnej, który zmienia postrzeganie pracodawców na znaczenie prac dyplomowych pisanych przez studentów uczelni wyższych.</p>
--- components/Footer.vue
+++ components/Footer.vue
@@ -1,44 +1,54 @@
 <template>
-  <footer class="fs16 section--full-padded">
-    <div class="container container--wide cols">
-      <div class="col col--two mb2 tl">
-        <b>Dodatkowe materiały</b><br>
-        <ul class="footer-list color-grey">
+  <footer class="text-sm leading-5 mt-10">
+    <div class="flex flex-col justify-between items-start sm:flex-row py-12 md:py-14 px-4 sm:px-6 lg:px-12">
+      <div>
+        <b>Dodatkowe materiały</b>
+        <ul class="text-gray-500">
           <li class="mb0">
-            <a href="https://anchor.fm/skuteczny-plan-podcast" target="_blank" title="Odsłuchaj podcast Skuteczny Plan na Spotify lub Anchor">
-              Odsłuchaj podcast <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12"><g fill="#8c8c8c"><path d="M8.293 2.293L4.586 6 6 7.414l3.707-3.707L12 6V0H6l2.293 2.293z"></path><path d="M10 10H2V2h2V0H2a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2V8h-2z"></path></g></svg>
+            <a href="https://anchor.fm/skuteczny-plan-podcast" target="_blank" title="Odsłuchaj podcast Skuteczny Plan na Spotify lub Anchor" class="flex items-center underline"> Odsłuchaj podcast 
+              <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" class="ml-1"><g fill="#8c8c8c"><path d="M8.293 2.293L4.586 6 6 7.414l3.707-3.707L12 6V0H6l2.293 2.293z"></path><path d="M10 10H2V2h2V0H2a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2V8h-2z"></path></g></svg>
             </a>
-          </li>
+          </li> 
           <li class="mb0">
-            <a href="http://medium.com/@leszekkrol" target="_blank" title="Skuteczny Plan Podcast na Medium">
-              Artukuły na Medium <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12"><g fill="#8c8c8c"><path d="M8.293 2.293L4.586 6 6 7.414l3.707-3.707L12 6V0H6l2.293 2.293z"></path><path d="M10 10H2V2h2V0H2a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2V8h-2z"></path></g></svg>
+            <a href="https://medium.com/@leszekkrol" target="_blank" title="Artykuły Leszek W. Król na Medium" class="flex items-center underline">
+              Artukuły na Medium 
+              <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" class="ml-1"><g fill="#8c8c8c"><path d="M8.293 2.293L4.586 6 6 7.414l3.707-3.707L12 6V0H6l2.293 2.293z"></path><path d="M10 10H2V2h2V0H2a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2V8h-2z"></path></g></svg>
             </a>
-          </li>
+          </li> 
           <li class="mb0">
-            <a href="http://linktr.ee/leszekkrol" target="_blank" title="Inne ciekawe projekty Leszek W. Król">
-              Inne projekty Leszka <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12"><g fill="#8c8c8c"><path d="M8.293 2.293L4.586 6 6 7.414l3.707-3.707L12 6V0H6l2.293 2.293z"></path><path d="M10 10H2V2h2V0H2a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2V8h-2z"></path></g></svg>
+            <a href="http://linktr.ee/leszekkrol" target="_blank" title="Inne ciekawe projekty Leszek W. Król" class="flex items-center underline">
+              Inne projekty Leszka 
+              <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" class="ml-1"><g fill="#8c8c8c"><path d="M8.293 2.293L4.586 6 6 7.414l3.707-3.707L12 6V0H6l2.293 2.293z"></path><path d="M10 10H2V2h2V0H2a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2V8h-2z"></path></g></svg>
             </a>
           </li>
         </ul>
-        <div class="mt2">Dumnie zasilany przez <a href="https://dhosting.pl/pp-leszekkrol" target="_blank" title="dhosting.pl wspiera podcast Skuteczny Plan prowadzony przez Leszek W. Król">dhosting.pl</a></div>
+        
+        <p class="mt-5 text-gray-500">
+          Dumnie zasilany przez <a href="https://dhosting.pl/pp-leszekkrol" target="_blank" title="dhosting.pl wspiera 4dnipracy.pl prowadzony przez Leszek W. Król" class="underline">dhosting.pl</a>
+        </p>
       </div>
-      <div class="col col--two mb2 tr">
-        <ul class="footer-list color-grey">
+      <div>
+        <ul class="text-gray-500 text-right">
           <li>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"><path d="M16 3c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4C.7 7.7 1.8 9 3.3 9.3c-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H0c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4C15 4.3 15.6 3.7 16 3z" fill="#575757"></path></svg> <a href="https://twitter.com/leszekkrol" target="_blank" title="Twitter Leszek W. Król">@leszekkrol</a>
-          </li>
+            <a href="https://twitter.com/leszekkrol" target="_blank" title="Twitter Leszek W. Król" class="flex items-center justify-end underline text-right">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="mr-1"><path d="M16 3c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4C.7 7.7 1.8 9 3.3 9.3c-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H0c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4C15 4.3 15.6 3.7 16 3z" fill="#575757"></path></svg> 
+              @leszekkrol
+            </a>
+          </li> 
           <li>
             <NuxtLink 
               :to="{ name: 'zasady-korzystania' }"
-              class="color-grey" 
+              class="underline" 
+              title="Zasady korzystnia"
               aria-label="Zasady korzystnia">Zasady</NuxtLink>
              · 
             <NuxtLink 
               :to="{ name: 'polityka-prywatnosci' }"
-              class="color-grey" 
+              class="underline" 
+              title="Polityka prywatności"
               aria-label="Polityka prywatności">Polityka</NuxtLink>
-          </li>
-          <li><small>© 2022 Leszek W. Król</small></li>
+          </li> 
+          <li><p class="pt-3">© 2022 Leszek W. Król</p></li>
         </ul>
       </div>
     </div>
