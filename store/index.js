833c3c9a25fc60c6333d2a9615e756b1e9418fe5
Leszek <leszek@strixspace.com>
2022-08-16 10:17:25 +0200

    🔖 Quotes updated

--- store/index.js
+++ store/index.js
@@ -80,24 +80,24 @@ export const state = () => ({
       position: "Projektanta"
     },
     {
-      description: "Leszka słucha się bardzo sympatycznie, delikatnie. Zdecydowanie podcast jest kierowany również do zwykłych ludzi, bez pompatycznych słów",
-      author: "Agnieszka",
-      position: "Studentka"
-    },
-    {
       description: "Leszek porusza tematy, ktore nie tylko związane są z przedsiębiorczością i biznesem, ale również mają związek z codziennym życiem.",
       author: "Jacek",
       position: "Student"
     },
     {
+      description: "Merytoryczny, inspirujący i rozwijający podcast. Dziękuję za wszystkie odcinki! Robisz mega robotę.",
+      author: "Paweł",
+      position: "Student"
+    },
+    {
+      description: "Podcast daje wiele odwagi do codziennego działania. Leszek rozwija i pomaga wyjść ze strefy komfortu pozwając przy tym myśleć i patrzeć szerzej.",
+      author: "Martyna",
+      position: "Grafik 2D"
+    },
+    {
       description: "Leszka słucha się bardzo sympatycznie, delikatnie. Zdecydowanie podcast jest kierowany również do zwykłych ludzi, bez pompatycznych słów",
       author: "Agnieszka",
       position: "Studentka"
-    },
-    {
-      description: "Przyznam że słuchając Ciebie tego dnia,  poczułam przypływ motywacji na  robienie rzeczy poza firmą",
-      author: "Joanna",
-      position: "Projektanta"
     },
   ]
 });
