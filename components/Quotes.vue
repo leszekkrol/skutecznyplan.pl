c819e96a12eab5ce04706a40c5443dac980bfc72
Leszek <leszek@strixspace.com>
2023-05-07 22:57:55 +0200

    :construction: Working on localization

--- components/Quotes.vue
+++ components/Quotes.vue
@@ -2,8 +2,8 @@
   <section class="py-20 sm:py-32">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div class="text-center mb-24">
-        <p class="text-sm font-semibold uppercase tracking-wide text-gray-300 sm:text-base lg:text-sm xl:text-base">Społeczność ⭐️</p>
-        <h2 class="mt-2 leading-loose font-extrabold tracking-tight text-header sm:text-4xl text-4xl sm:leading-10 lg:leading-relaxed lg:text-6xl">Co mówią słuchacze?</h2>
+        <p class="text-sm font-semibold uppercase tracking-wide text-gray-300 sm:text-base lg:text-sm xl:text-base">{{ $t('common.community') }}</p>
+        <h2 class="mt-2 leading-loose font-extrabold tracking-tight text-header sm:text-4xl text-4xl sm:leading-10 lg:leading-relaxed lg:text-6xl">{{ $t('common.what-listeners-saying') }}</h2>
         <p class="mt-4 max-w-2xl text-xl leading-7 text-gray-300 mx-auto">Moja społeczność to każdy, kto chce zwiększyć swoją produktywność i efektywność podczas codziennej pracy. Siegają po tą trudno dostępną, praktyczną i unikalną wiedzę: twórcy, pisarze, programiści, marketerzy, agencje, firmy programistyczne, społeczności, podcasterzy...</p>
       </div>
       <ul class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-6 sm:gap-8 lg:mt-20 lg:max-w-none lg:grid-cols-3">
