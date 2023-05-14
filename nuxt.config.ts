86cf038515c0e7421d012892dd99ec75a36592ef
Leszek <leszek@strixspace.com>
2023-05-14 22:58:59 +0200

    🛠  New approach to static 404 page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  generate: {
+    fallback: true,
+  },
   app: {
     head: {
       title: "Skuteczny Plan - Rozwiń praktyczne umiejętności zarządzania",
