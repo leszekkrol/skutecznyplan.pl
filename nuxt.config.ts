719ad6c0d621fa33a8d837b3a4dc3c88ef598c48
Leszek <leszek@strixspace.com>
2023-05-15 08:53:13 +0200

    :bug: Fix file name issue for [content:en:it's-not-the-goal-that-counts.md]

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  generate: {
-    fallback: true,
-  },
   app: {
     head: {
       title: "Skuteczny Plan - Rozwiń praktyczne umiejętności zarządzania",
