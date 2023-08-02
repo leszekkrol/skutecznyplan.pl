a1cab00b7e9f0770086ad7d0143e68425f434c0a
Leszek <leszek@strixspace.com>
2023-08-02 19:39:02 +0200

    :star: Updated author description content

--- components/Author.vue
+++ components/Author.vue
@@ -18,50 +18,13 @@
         </p>
         <p class="mt-4 text-lg tracking-tight">
           {{ $t('section.author.description_2') }}
-
-          <a 
-            href="https://universityofgames.net" 
-            target="_blank" 
-            data-splitbee-event="University of Games"
-            data-splitbee-event-destination="universityofgames.net"
-            title="University of Games - Społeczność twórców gier" 
-            class="underline">
-              <strong>University of Games</strong><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" class="inline ml-1"><g fill="#8c8c8c"><path d="M8.293 2.293L4.586 6 6 7.414l3.707-3.707L12 6V0H6l2.293 2.293z"></path><path d="M10 10H2V2h2V0H2a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2V8h-2z"></path></g></svg>
-          </a> 
-
-          {{ $t('section.author.description_3') }} 
         </p>
         <p class="mt-4 text-lg tracking-tight">
-          {{ $t('section.author.description_4') }}  
-          <a 
-            href="https://napiszprace.pl" 
-            target="_blank" 
-            data-splitbee-event="Napisz Prace"
-            data-splitbee-event-destination="napiszprace.pl"
-            title="Napisz Pracę - Wybierz jedno z setek wyzwań z którymi mierzą się przedsiębiorstwa w twojej okolicy." 
-            class="underline"><strong>NapiszPrace.pl</strong><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" class="inline ml-1"><g fill="#8c8c8c"><path d="M8.293 2.293L4.586 6 6 7.414l3.707-3.707L12 6V0H6l2.293 2.293z"></path><path d="M10 10H2V2h2V0H2a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2V8h-2z"></path></g></svg>
-          </a>
-
-          {{ $t('section.author.description_5') }}
+          {{ $t('section.author.description_3') }}  
         </p>
         <p class="mt-4 text-lg tracking-tight">
-          {{ $t('section.author.description_6') }}
-          <a 
-            href="https://hanzea.tech" 
-            target="_blank" 
-            data-splitbee-event="Hanzea Tech"
-            data-splitbee-event-destination="hanzea.tech"
-            title="Hanzea.Tech - rozwiązania dla fintech" 
-            class="underline"><strong>HanzeaTech</strong><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" class="inline ml-1"><g fill="#8c8c8c"><path d="M8.293 2.293L4.586 6 6 7.414l3.707-3.707L12 6V0H6l2.293 2.293z"></path><path d="M10 10H2V2h2V0H2a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2V8h-2z"></path></g></svg>
-          </a>
-          {{ $t('section.author.description_7') }} 
+          {{ $t('section.author.description_4') }}
         </p>
-        <p class="mt-4 text-lg tracking-tight">
-          <span class="font-bold underline">{{ $t('section.author.description_8') }}</span> {{ $t('section.author.description_9') }}
-        </p>
-        <p class="mt-4 text-lg tracking-tight">{{ $t('section.author.description_10') }}</p>
-        <p class="mt-4 text-lg tracking-tight">{{ $t('section.author.description_11') }}</p>
-        <p class="mt-4 text-lg tracking-tight">{{ $t('section.author.description_12') }}</p>
         <img src="~/assets/img/sign.png" alt="Leszek W. Król - Przedsiębiorca" height="10" class="mt-10">
       </div>
     </div>
